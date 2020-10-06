63230433e41c5ab4fc888d00b0f13891f787d79c
Thyago Mendes <thyagomendes@MacBook-Pro-de-Thyago.local>
2020-10-05 22:42:34 -0300

    Backend da Aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const { request, response } = require('express');
+const cors = require ('cors');
+const express = require('express');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
